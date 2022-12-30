1d4b54a6d07641c3b89acc20b16807bbe823856a
PravinElangovan <pravinkumarelangovan20@gmail.com>
2022-12-30 14:24:36 +0530

    Changes Done

--- src/main/java/com/driver/StudentController.java
+++ src/main/java/com/driver/StudentController.java
@@ -33,7 +33,7 @@ public class StudentController {
     }
 
     @PutMapping("/add-student-teacher-pair")
-    public ResponseEntity<String> addStudentTeacherPair(@RequestParam String student, @RequestParam String teacher){
+    public ResponseEntity<String> addStudentTeacherPair(@RequestParam("name") String student, @RequestParam("name") String teacher){
         studentService.studentTeacherPair(student,teacher);
         return new ResponseEntity<>("New student-teacher pair added successfully", HttpStatus.CREATED);
     }
